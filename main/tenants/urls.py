78e3a33aa8163cbff56e0bb8e26ebe06e7f13db6
variadec <zayyadidris294@gmail.com>
2024-09-02 07:46:43 +0200

    fixed home issues

--- main/tenants/urls.py
+++ main/tenants/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = (
     [
         path("", views.HomeView.as_view(), name="home"),
